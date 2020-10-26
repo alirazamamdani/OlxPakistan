515d2b67abf8681b908a32c9c3f30170b34e9418
alirazamamdani <ali.mamdani@Hotmail.com>
2020-10-26 20:03:47 +0500

    create olx

---
+++ src/components/header.jsx
@@ -0,0 +1,19 @@
+import React,  {Component} from 'react';
+import './css/header.css'
+
+
+class Header extends Component{
+    render(props) {
+        return(
+            <div className="head-container">
+                <div className="head">
+                    
+                </div>
+            </div>
+        
+      )
+    } 
+    
+ }
+
+ export default Header;
